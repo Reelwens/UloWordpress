43b8db6faf6ee21e5ad5234634e8ce78295e0cdb
Simon LUCAS <sim.lucas27@gmail.com>
2017-11-20 17:32:17 +0100

    :sparkles: Integrate Homepage

--- wp-content/themes/front_no_wordpress/src/index.php
+++ wp-content/themes/front_no_wordpress/src/index.php
@@ -23,14 +23,139 @@
     <meta property="og:description" content="Ulo est une caméra de surveillance mignonne représentée par une chouette." />
     <meta property="og:site_name" content="Ulo — Caméra de surveillance" />
 
-    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500, 600" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link href="css/app.min.css" rel="stylesheet" type="text/css" />
 </head>
-<body>
+<body id="home">
+
+    <div class="landing">
+        <div class="container">
+
+            <div class="menu">
+                <div class="menuItem active">
+                    <a href="#">Accueil</a>
+                </div>
+                <div class="menuItem">
+                    <a href="#">Fonctionnalités</a>
+                </div>
+                <div class="menuItem">
+                    <a href="#">Presse</a>
+                </div>
+                <div class="menuItem">
+                    <a href="#">Témoignages</a>
+                </div>
+                <div class="menuItem">
+                    <a href="#">Contact</a>
+                </div>
+            </div>
+
+            <h1>ULO, une chouette caméra de surveillance</h1>
+            <div class="head-button"><a href="#">En savoir plus</a></div>
+
+        </div>
+
+        <img src="/img/down-arrow.svg" alt="Descendre" class="downArrow">
+    </div>
+
+    <div class="presentation">
+        <div class="container">
+
+            <div class="title">
+                <h2>Présentation</h2>
+                <div class="line"></div>
+            </div>
+
+            <p class="content">Ulo est une adorable caméra de surveillance une chouette interargissant avec vous à travers l’expression de ses yeux.<br /><br />Aujourd’hui de plus en plus d’objets sont connectés, mais aucun ne connecte vraiment avec vous</p>
+        </div>
+
+        <div class="picturesPresentation">
+            <img src="/img/ULO-image4.jpg" alt="Présentation Ulo">
+            <img src="/img/ULO-image9.jpg" alt="Présentation Ulo 2">
+        </div>
+    </div>
+
+    <div class="features">
+        <div class="background">
+            <div class="bkgTop"></div>
+            <img src="/img/bkgArrow.svg" alt="background">
+        </div>
+
+        <div class="floatGroup container">
+            <div class="title">
+                <h2>Fonctionnalités</h2>
+                <div class="line"></div>
+            </div>
+
+            <div class="featuresPresentation">
+                <div class="feature">
+                    <img src="/img/noun_192274_cc.svg" alt="Camera">
+                    <h3>Surveillance</h3>
+                    <p class="hidden-xs">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci laudantium quia necessitatibus aliquam repellat pariatur reiciendis. Tenetur laborum dolor obcaecati tempora. Cupiditate ducimus consequatur excepturi quo cum rerum aliquam earum</p>
+                </div>
+
+                <div class="feature">
+                    <img src="/img/noun_1129813_cc.svg" alt="Camera">
+                    <h3>Sécurité</h3>
+                    <p class="hidden-xs">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci laudantium quia necessitatibus aliquam repellat pariatur reiciendis. Tenetur laborum dolor obcaecati tempora. Cupiditate ducimus consequatur excepturi quo cum rerum aliquam earum</p>
+                </div>
+
+                <div class="feature">
+                    <img src="/img/noun_41030_cc.svg" alt="Camera">
+                    <h3>Domotique</h3>
+                    <p class="hidden-xs">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci laudantium quia necessitatibus aliquam repellat pariatur reiciendis. Tenetur laborum dolor obcaecati tempora. Cupiditate ducimus consequatur excepturi quo cum rerum aliquam earum</p>
+                </div>
+            </div>
+
+            <div class="buttonContainer"><div class="button"><a href="">Découvrir</a></div></div>
+        </div>
+    </div>
+
+    <div class="catchLines">
+        <h3>Utilisable Partout</h3>
+        <p>Elle peut être utilisée à domicile, dans un bureau, ou tout autre endroit du quotidien.<br />Il suffit de mettre sur le mur la petite magnette pour la tenir. Vous êtes rentré à la maison ou vous allez partir au boulot ?<br />C’est donc le moment d’allumer ou d’éteindre Ulo.</p>
+    </div>
 
 
+    <footer>
+        <div class="mainFooter container">
 
+            <div class="contact">
+                <div class="title">
+                    <h2>Contact</h2>
+                    <div class="line"></div>
+
+                    <div class="contactBlocks">
+                        <div class="contactBlock">
+                            <img src="/img/noun_1146595_cc.svg" alt="Localisation">
+                            <p>9 AVENUE DES HAUTS-FOURNEAUX<br />4362 ESCH-SUR-ALZETTE<br />LUXEMBOURG</p>
+                        </div>
+                        <div class="contactBlock">
+                            <img src="/img/noun_939012_cc.svg" alt="Mail">
+                            <p>INFO@MU-DESIGN.LU</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="newsletter">
+                <div class="title">
+                    <h2>Newsletter</h2>
+                    <div class="line"></div>
+                </div>
+
+                <p class="mailAnswer">Entrez votre adresse e-mail pour<br />recevoir nos dernières actualités.</p>
+
+                <input type="text" class="mailInput" placeholder="Adresse mail">
+                <div class="button"><a href="#">Je m'inscris</a></div>
+            </div>
+
+        </div>
+
+        <div class="map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.36293684786!2d2.417315350998301!3d48.85128920905941!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e67283a4697951%3A0x582f2d404081561e!2s8+Rue+de+Valmy%2C+93100+Montreuil!5e0!3m2!1sfr!2sfr!4v1511189721071" width="100%" height="250" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+    </footer>
 <!-- Scripts -->
 <script src="js/script.js" charset="utf-8"></script>
 
