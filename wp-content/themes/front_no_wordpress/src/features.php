ec44aebb4f65c67fd132e28c12649eab2cb3d66b
Simon LUCAS <sim.lucas27@gmail.com>
2017-11-22 10:23:55 +0100

    :sparkles: Feature page

--- wp-content/themes/front_no_wordpress/src/features.php
+++ wp-content/themes/front_no_wordpress/src/features.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <title>Ulo — Votre nouvelle caméra de sécurité</title>
+    <title>Ulo — Fonctionnalités</title>
     <meta name="description" content="Ulo est une caméra de surveillance mignonne représentée par une chouette." />
     <link rel="icon" type="image/png" href="img/logo.png" sizes="64x64">
 
@@ -27,16 +27,16 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link href="css/app.min.css" rel="stylesheet" type="text/css" />
 </head>
-<body id="home">
+<body id="features">
 
     <div class="landing">
         <div class="container">
 
             <div class="menu">
-                <div class="menuItem active">
+                <div class="menuItem">
                     <a href="index.php">Accueil</a>
                 </div>
-                <div class="menuItem">
+                <div class="menuItem active">
                     <a href="features.php">Fonctionnalités</a>
                 </div>
                 <div class="menuItem">
@@ -50,28 +50,19 @@
                 </div>
             </div>
 
-            <h1>ULO, une chouette caméra de surveillance</h1>
-            <div class="head-button"><a href="#">En savoir plus</a></div>
+            <h1>Fonctionnalités</h1>
 
         </div>
-
-        <img src="../img/down-arrow.svg" alt="Descendre" class="downArrow">
     </div>
 
-    <div class="presentation">
+    <div class="surveillance">
         <div class="container">
 
             <div class="title">
-                <h2>Présentation</h2>
+                <h2>Surveillance</h2>
                 <div class="line"></div>
             </div>
 
-            <p class="content">Ulo est une adorable caméra de surveillance une chouette interargissant avec vous à travers l’expression de ses yeux.<br /><br />Aujourd’hui de plus en plus d’objets sont connectés, mais aucun ne connecte vraiment avec vous</p>
-        </div>
-
-        <div class="picturesPresentation">
-            <img src="img/ULO-image4.jpg" alt="Présentation Ulo">
-            <img src="img/ULO-image9.jpg" alt="Présentation Ulo 2">
         </div>
     </div>
 
@@ -82,38 +73,94 @@
         </div>
 
         <div class="floatGroup container">
-            <div class="title">
-                <h2>Fonctionnalités</h2>
-                <div class="line"></div>
+            <div class="heading">
+                <img src="img/Vector.svg" alt="Caméra">
+                <p>En live</p>
             </div>
+            <p class="bottomBorder">Il est très facile de voir ce qui se passe dans une autre pièce ou lorsque vous êtes sortis.<br />Ulo enregistre en direct (audio et vidéo) et le diffuse sur une page web sécurisée ou une application mobile.<br />Cette fonctionnalité lui permet notamment d’être utilisée comme caméra pour bébé.</p>
 
-            <div class="featuresPresentation">
-                <div class="feature">
-                    <img src="img/noun_192274_cc.svg" alt="Camera">
-                    <h3>Surveillance</h3>
-                    <p class="hidden-xs">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci laudantium quia necessitatibus aliquam repellat pariatur reiciendis. Tenetur laborum dolor obcaecati tempora. Cupiditate ducimus consequatur excepturi quo cum rerum aliquam earum</p>
-                </div>
-
-                <div class="feature">
-                    <img src="img/noun_1129813_cc.svg" alt="Camera">
-                    <h3>Sécurité</h3>
-                    <p class="hidden-xs">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci laudantium quia necessitatibus aliquam repellat pariatur reiciendis. Tenetur laborum dolor obcaecati tempora. Cupiditate ducimus consequatur excepturi quo cum rerum aliquam earum</p>
-                </div>
-
-                <div class="feature">
-                    <img src="img/noun_41030_cc.svg" alt="Camera">
-                    <h3>Domotique</h3>
-                    <p class="hidden-xs">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci laudantium quia necessitatibus aliquam repellat pariatur reiciendis. Tenetur laborum dolor obcaecati tempora. Cupiditate ducimus consequatur excepturi quo cum rerum aliquam earum</p>
-                </div>
+            <div class="heading">
+                <img src="img/exclamation.svg" alt="Alerte">
+                <p>Alert mode</p>
             </div>
+            <p class="bottomBorder">Un niveau supplémentaire de sécurité a été créé, l’Alert Mode, celui-ci s’enclenche en tapotant 2 fois la tête de la chouette.<br />Pendant ce mode, à chaque mouvement distingué, Ulo envoie un mail avec une photode ce qu’il s’est passé. Ce mode se désactive dès que votre smartphone est sur le même réseau Wifi que Ulo.</p>
 
-            <div class="buttonContainer"><div class="button"><a href="">Découvrir</a></div></div>
+            <div class="heading">
+                <img src="img/moon-and-stars.svg" alt="Nocturne">
+                <p>Vision nocturne</p>
+            </div>
+            <p class="bottomBorder">Voit la nuit grâce à sa vision nocturne.</p>
+
+            <div class="heading">
+                <img src="img/objective-searching.svg" alt="Regard">
+                <p>Suivi du regard</p>
+            </div>
+            <p>Cette caméra Wi-Fi est tout à fait unique en son genre par le simple fait qu’elle ne se limite pas à surveiller à distance ou à enregistrer des images qui peuvent être visionnées en différé, mais elle peut aussi vous suivre par son regard.</p>
         </div>
     </div>
 
-    <div class="catchLines">
-        <h3>Utilisable Partout</h3>
-        <p>Elle peut être utilisée à domicile, dans un bureau, ou tout autre endroit du quotidien.<br />Il suffit de mettre sur le mur la petite magnette pour la tenir. Vous êtes rentré à la maison ou vous allez partir au boulot ?<br />C’est donc le moment d’allumer ou d’éteindre Ulo.</p>
+    <div class="sécurité">
+        <div class="container">
+
+            <div class="title">
+                <h2>Sécurité</h2>
+                <div class="line"></div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="features">
+        <div class="background">
+            <div class="bkgTop"></div>
+            <img src="img/bkgArrow.svg" alt="background">
+        </div>
+
+        <div class="floatGroup container">
+            <div class="heading">
+                <img src="img/noun_1129813_cc.svg" alt="Cadenas">
+                <p>Protection</p>
+            </div>
+            <p class="bottomBorder">Ulo est un petit serveur qui stocke tout ce qu’il fait quand il en a besoin. Rien est envoyé sur un serveur tiers. Ulo envoie des données sur les différents devices que lorsque c’est nécessaire. C’est aussi très simple d’installation.<br />Dès que vous voulez rajouter un device qui accède à Ulo, cela génère un QR Code qu’il faut montrer à Ulo pour associer le device à Ulo.<br />Pour plus de sécurité, tous les 6 mois, cette action est à refaire.</p>
+
+            <div class="heading">
+                <img src="img/share.svg" alt="Share">
+                <p>Compatibilité</p>
+            </div>
+            <p>Les services Ulo sont disponibles sur une page web. Ainsi Ulo est compatible avec tout appareil avec une connexion internet et un navigateur web. Il y a aussi une application mobile dédiée sur Android et iOS.</p>
+        </div>
+    </div>
+
+    <div class="domotique">
+        <div class="container">
+
+            <div class="title">
+                <h2>Domotique</h2>
+                <div class="line"></div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="features">
+        <div class="background">
+            <div class="bkgTop"></div>
+            <img src="img/bkgArrow.svg" alt="background">
+        </div>
+
+        <div class="floatGroup container">
+            <div class="heading">
+                <img src="img/eye.svg" alt="Cadenas">
+                <p>Personnalisation</p>
+            </div>
+            <p class="bottomBorder">Ulo communique par l’expression de ses yeux. La page internet ou l’application mobile vous permettent de personnaliser la couleur des yeux, leur forme et leur taille pour vous plaire et se fondre dans le décor. Aucun logo ou icones sont affichés à l’écran mais vous savez tout de suite ce que Ulo a en tête.</p>
+
+            <div class="heading">
+                <img src="img/rain.svg" alt="Share">
+                <p>Changement d'humeur</p>
+            </div>
+            <p>Le temps à l’extérieur peut modifier le comportement d’Ulo. Par exemple, en cas de pluie, Ulo devient grognon. De plus, elle est résistante à l’eau et peut être placée à l’extérieur dans des températures allant de -1O °C à 50°C.</p>
+        </div>
     </div>
 
 
