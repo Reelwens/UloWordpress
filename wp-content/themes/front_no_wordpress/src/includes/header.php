17576a6280329760ec0396acd81a223e0ee3b46f
Simon LUCAS <sim.lucas27@gmail.com>
2017-11-25 19:29:39 +0100

    :sparkles: Include header/footer

---
+++ wp-content/themes/front_no_wordpress/src/includes/header.php
@@ -0,0 +1,25 @@
+<div class="landing">
+    <div class="container">
+
+        <div class="menu">
+            <div class="menuItem">
+                <a href="index.php">Accueil</a>
+            </div>
+            <div class="menuItem">
+                <a href="features.php">Fonctionnalités</a>
+            </div>
+            <div class="menuItem">
+                <a href="press.php">Presse</a>
+            </div>
+            <div class="menuItem">
+                <a href="testimony.php">Témoignages</a>
+            </div>
+            <div class="menuItem">
+                <a href="testimony.php#contact">Contact</a>
+            </div>
+        </div>
+
+        <h1>Fonctionnalités</h1>
+
+    </div>
+</div>
--- wp-content/themes/front_no_wordpress/src/index.php
+++ wp-content/themes/front_no_wordpress/src/index.php
@@ -116,46 +116,7 @@
         <p>Elle peut être utilisée à domicile, dans un bureau, ou tout autre endroit du quotidien.<br />Il suffit de mettre sur le mur la petite magnette pour la tenir. Vous êtes rentré à la maison ou vous allez partir au boulot ?<br />C’est donc le moment d’allumer ou d’éteindre Ulo.</p>
     </div>
 
-
-    <footer>
-        <div class="mainFooter container">
-
-            <div class="contact">
-                <div class="title">
-                    <h2>Contact</h2>
-                    <div class="line"></div>
-
-                    <div class="contactBlocks">
-                        <div class="contactBlock">
-                            <img src="img/noun_1146595_cc.svg" alt="Localisation">
-                            <p>9 AVENUE DES HAUTS-FOURNEAUX<br />4362 ESCH-SUR-ALZETTE<br />LUXEMBOURG</p>
-                        </div>
-                        <div class="contactBlock">
-                            <img src="img/noun_939012_cc.svg" alt="Mail">
-                            <p>INFO@MU-DESIGN.LU</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="newsletter">
-                <div class="title">
-                    <h2>Newsletter</h2>
-                    <div class="line"></div>
-                </div>
-
-                <p class="mailAnswer">Entrez votre adresse e-mail pour<br />recevoir nos dernières actualités.</p>
-
-                <input type="text" class="mailInput" placeholder="Adresse mail">
-                <div class="button"><a href="#">Je m'inscris</a></div>
-            </div>
-
-        </div>
-
-        <div class="map">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.36293684786!2d2.417315350998301!3d48.85128920905941!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e67283a4697951%3A0x582f2d404081561e!2s8+Rue+de+Valmy%2C+93100+Montreuil!5e0!3m2!1sfr!2sfr!4v1511189721071" width="100%" height="250" frameborder="0" style="border:0" allowfullscreen></iframe>
-        </div>
-    </footer>
+    <?php include('includes/footer.php') ?>
 <!-- Scripts -->
 <script src="js/script.js" charset="utf-8"></script>
 
