67ec9e2e853d1a8e864ff34f79e069d6a58d5fb5
Simon LUCAS <sim.lucas27@gmail.com>
2017-11-25 19:38:38 +0100

    :sparkles: Work on include header

--- wp-content/themes/front_no_wordpress/src/press.php
+++ wp-content/themes/front_no_wordpress/src/press.php
@@ -1,34 +1,3 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-    <title>Ulo — Presse</title>
-    <meta name="description" content="Ulo est une caméra de surveillance mignonne représentée par une chouette." />
-    <link rel="icon" type="image/png" href="img/logo.png" sizes="64x64">
-
-    <!-- Twitter Card data -->
-    <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:title" content="Ulo — Caméra de surveillance">
-    <meta name="twitter:description" content="Ulo est une caméra de surveillance mignonne représentée par une chouette.">
-    <!-- Twitter summary card with large image must be at least 280x150px -->
-    <meta name="twitter:image:src" content="http://image.noelshack.com/fichiers/2017/46/7/1511111565-home-header.png">
-
-    <!-- Open Graph data -->
-    <meta property="og:title" content="Ulo — Caméra de surveillance" />
-    <meta property="og:type" content="website" />
-    <meta property="og:image" content="http://image.noelshack.com/fichiers/2017/46/7/1511111565-home-header.png" />
-    <meta property="og:description" content="Ulo est une caméra de surveillance mignonne représentée par une chouette." />
-    <meta property="og:site_name" content="Ulo — Caméra de surveillance" />
-
-    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700" rel="stylesheet">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link href="css/app.min.css" rel="stylesheet" type="text/css" />
-</head>
-<body id="press">
-
     <?php include('includes/header.php'); ?>
 
     <div class="features">
